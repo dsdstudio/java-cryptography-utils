b8226e9215c562972bd2d67a35c4a032ae1b792b
보롱킴 <dsdgun@gmail.com>
2016-07-10 02:16:17 +0900

    함수들 실제 사용하는 클래스로 이동, 함수 네이밍 정리

--- src/main/java/net/dsdstudio/crypto/CryptoUtil.java
+++ src/main/java/net/dsdstudio/crypto/CryptoUtil.java
@@ -1,4 +1,61 @@
 package net.dsdstudio.crypto;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Base64;
+
 public class CryptoUtil {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static String aes256Decrypt(String hexEncodedText, String secretKey) {
+        try {
+            String iv = secretKey.substring(0, 16);
+            byte[] keyData = Base64.getDecoder().decode(secretKey);
+
+            SecretKey secureKey = new SecretKeySpec(keyData, "AES");
+            Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            c.init(Cipher.DECRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes("UTF-8")));
+
+            byte[] byteStr = Hex.decode(hexEncodedText);
+            return new String(c.doFinal(byteStr), "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String aes256Encrypt(String text, String secretKey) {
+        try {
+            String iv = secretKey.substring(0, 16);
+            byte[] keyData = Base64.getDecoder().decode(secretKey);
+
+            SecretKey secureKey = new SecretKeySpec(keyData, "AES");
+
+            Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            c.init(Cipher.ENCRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes()));
+
+            byte[] encrypted = c.doFinal(text.getBytes("UTF-8"));
+            return Hex.toHexString(encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String sha256Hash(byte[] input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+            return Hex.toHexString(md.digest(input));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/net/dsdstudio/crypto/CryptoUtilTest.java
+++ src/test/java/net/dsdstudio/crypto/CryptoUtilTest.java
@@ -1,77 +1,29 @@
 package net.dsdstudio.crypto;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.util.encoders.Hex;
 import org.junit.Test;
 
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.Base64;
 
 /**
  * Created by bhkim on 2016. 7. 10..
  */
 public class CryptoUtilTest {
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
-
     @Test
     public void test() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchProviderException, NoSuchPaddingException {
         String plainText = "우허허허허";
-        String key = Base64.getEncoder().encodeToString("0123456789123456".getBytes());
+        String secretKey = Base64.getEncoder().encodeToString("0123456789123456".getBytes());
 
-        String encoded = encrypt(plainText, key);
+        String encoded = CryptoUtil.aes256Encrypt(plainText, secretKey);
         System.out.println("original text :: " + plainText);
         System.out.println("encrypted :: " + encoded);
-        System.out.println("decrypted :: " + decrypt(encoded, key));
+        System.out.println("decrypted :: " + CryptoUtil.aes256Decrypt(encoded, secretKey));
 
-        System.out.println("sha256hash :: " + sha256Hash(plainText.getBytes()));
-    }
-
-
-    private String decrypt(String hexEncodedText, String secretKey) {
-        try {
-            String iv = secretKey.substring(0, 16);
-            byte[] keyData = Base64.getDecoder().decode(secretKey);
-
-            SecretKey secureKey = new SecretKeySpec(keyData, "AES");
-            Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            c.init(Cipher.DECRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes("UTF-8")));
-
-            byte[] byteStr = Hex.decode(hexEncodedText);
-            return new String(c.doFinal(byteStr), "UTF-8");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String encrypt(String text, String secretKey) {
-        try {
-            String iv = secretKey.substring(0, 16);
-            byte[] keyData = Base64.getDecoder().decode(secretKey);
-
-            SecretKey secureKey = new SecretKeySpec(keyData, "AES");
-
-            Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            c.init(Cipher.ENCRYPT_MODE, secureKey,
-                    new IvParameterSpec(iv.getBytes()));
-
-            byte[] encrypted = c.doFinal(text.getBytes("UTF-8"));
-            return Hex.toHexString(encrypted);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String sha256Hash(byte[] input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA256");
-            return Hex.toHexString(md.digest(input));
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("sha256hash :: " + CryptoUtil.sha256Hash(plainText.getBytes()));
     }
 }
