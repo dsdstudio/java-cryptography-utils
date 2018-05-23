3f55eb4df748d6ce13b6f1951f5e4caf1723dedb
Bohyung Kim <dsdgun@gmail.com>
2018-05-24 03:45:59 +0900

    generate certificate #2

--- src/test/java/net/dsdstudio/crypto/CertificateTest.java
+++ src/test/java/net/dsdstudio/crypto/CertificateTest.java
@@ -6,15 +6,22 @@ import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.security.Security;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.Date;
 
 public class CertificateTest {
@@ -26,10 +33,8 @@ public class CertificateTest {
     }
 
     @Test
-    public void 인증서생성테스트() throws Exception {
-        KeyPair pair = CertUtils.generateKeyPair("RSA", 4096);
-
-        SubjectPublicKeyInfo bcPk = SubjectPublicKeyInfo.getInstance(pair.getPublic().getEncoded());
+    public void 셀프사인_인증서생성테스트() throws Exception {
+        KeyPair pair = CertUtils.generateKeyPair("ECDSA", 256);
 
         X500Name issuer = new X500NameBuilder()
                 .addRDN(BCStyle.C, "KR")
@@ -54,5 +59,12 @@ public class CertificateTest {
         builder.addExtension(Extension.subjectKeyIdentifier, false, extUtils.createSubjectKeyIdentifier(pair.getPublic()));
         builder.addExtension(Extension.authorityKeyIdentifier, false, extUtils.createAuthorityKeyIdentifier(pair.getPublic()));
 
+        ContentSigner contentSigner = new JcaContentSignerBuilder("SHA512WithECDSA").build(pair.getPrivate());
+        X509Certificate selfSignedCert = new JcaX509CertificateConverter().getCertificate(builder.build(contentSigner));
+
+        System.out.println(selfSignedCert);
+        String certString = "-----BEGIN CERTIFICATE-----\n" + Base64.getEncoder().encodeToString(selfSignedCert.getEncoded())
+                + "\n-----END CERTIFICATE-----";
+        Files.write(Paths.get("test.crt"), certString.getBytes());
     }
 }
